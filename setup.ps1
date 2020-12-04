85cbc9d8477a23e9cd8772d4d0f21d74c3d23f5b
Nadir Badnjevic <33620914+nadirbad@users.noreply.github.com>
2020-12-04 22:37:16 +0100

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,62 @@
+function Install-Chocolatey {
+    Set-ExecutionPolicy Bypass -Scope Process -Force;
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+function Install-FromChocolatey {
+    param(
+        [string]
+        [Parameter(Mandatory = $true)]
+        $PackageName
+    )
+
+    choco install $PackageName --yes
+}
+
+function Install-PowerShellModule {
+    param(
+        [string]
+        [Parameter(Mandatory = $true)]
+        $ModuleName,
+
+        [ScriptBlock]
+        [Parameter(Mandatory = $true)]
+        $PostInstall = {}
+    )
+
+    if (!(Get-Command -Name $ModuleName -ErrorAction SilentlyContinue)) {
+        Write-Host "Installing $ModuleName"
+        Install-Module -Name $ModuleName -Scope CurrentUser -Confirm $true
+        Import-Module $ModuleName -Confirm
+
+        Invoke-Command -ScriptBlock $PostInstall
+    } else {
+        Write-Host "$ModuleName was already installed, skipping"
+    }
+}
+
+Install-Chocolatey
+
+Install-FromChocolatey 'git'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/nadirbad/system-init/master/.gitconfig' -OutFile (Join-Path $env:USERPROFILE '.gitconfig')
+
+Install-FromChocolatey 'vscode'
+Install-FromChocolatey 'dotnetcore-sdk'
+Install-FromChocolatey 'microsoft-windows-terminal'
+Install-FromChocolatey 'fiddler'
+Install-FromChocolatey 'postman'
+#Install-FromChocolatey 'linqpad'
+Install-FromChocolatey 'firefox'
+Install-FromChocolatey 'googlechrome'
+Install-FromChocolatey 'powershell-core'
+
+Install-PowerShellModule 'Posh-Git' { Add-PoshGitToProfile -AllHosts }
+Install-PowerShellModule 'oh-my-posh' { }
+Install-PowerShellModule 'PSReadLine' { }
+Install-PowerShellModule 'nvm' {
+    Install-NodeVersion latest
+    Set-NodeVersion -Persist User latest
+}
+
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/nadirbad/system-init/master/Microsoft.PowerShell_profile.ps1' -OutFile $PROFILE
